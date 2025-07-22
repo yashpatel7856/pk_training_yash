9213dfa905fa6eb80954e4f46001ff8a56f6e8f0
yashpatel7856 <120588546+yashpatel7856@users.noreply.github.com>
2025-07-22 15:52:42 +0530

    after replacing sort with for loop

--- d-2/1_sort_object_keys_22_07.js
+++ d-2/1_sort_object_keys_22_07.js
@@ -8,28 +8,49 @@
 //start time : 10:27
 //end time : 10:44
 
-let obj = {"a": "b", "b": "a", "c": "c"}
+let obj = {"a": 1, "b": 3, "c": 2}
 // let obj={"b": 2,"d":"a","e":3, "a": "A", "c": 1,"f":"B"}
 
 function getSortedKeysObject(obj){
     let sorted;
     let temp=Object.entries(obj);
-    
+    let tempObj={}
     //to sort mixed value of numbers and aplhabets (1A2aB)
     // sorted=temp.sort((a,b)=>{
     //     return a[1].toString().charCodeAt(0)-b[1].toString().charCodeAt(0)
     // })
 
     if(typeof temp[0][1]=="number"){
-         sorted=temp.sort((a,b)=>{
-            return a[1]-b[1]
-        })
+        //  sorted=temp.sort((a,b)=>{
+        //     return a[1]-b[1]
+        // })
+
+        for(let i=0;i<temp.length;i++){
+            for(let j=0;j<temp.length-1;j++){                
+                if(temp[j][1]>temp[i][1]){
+                    let t=temp[j];
+                    temp[j]=temp[i];
+                    temp[i]=t
+                }
+            }
+        }
+        sorted=temp
+
     }else{
-         sorted=temp.sort((a,b)=>{
-            return a[1].charCodeAt(0)-b[1].charCodeAt(0)
-        })
+        //  sorted=temp.sort((a,b)=>{
+        //     return a[1].charCodeAt(0)-b[1].charCodeAt(0)
+        // })
+        for(let i=0;i<temp.length;i++){
+            for(let j=0;j<temp.length-1;j++){                
+                if(temp[j][1].toString().charCodeAt(0)>temp[i][1].toString().charCodeAt(0)){
+                    let t=temp[j];
+                    temp[j]=temp[i];
+                    temp[i]=t
+                }
+            }
+        }
     }
-    return Object.fromEntries(sorted)
+    return Object.fromEntries(temp)
 }
 
 console.log(getSortedKeysObject(obj))
