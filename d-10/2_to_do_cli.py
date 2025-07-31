c26a2f734795d5a1e7394076be4ff069cd47f5ef
yashpatel7856 <120588546+yashpatel7856@users.noreply.github.com>
2025-07-31 13:48:25 +0530

    added given changes for multiple user

--- d-10/2_to_do_cli.py
+++ d-10/2_to_do_cli.py
@@ -15,60 +15,35 @@ class Task:
     
 def startTodoFucntion():
     defaultFileName="todo_data.json"
-    taskHistory={
-        "u1":[],
-        "u2":[],
-        "u1TaskNumberList":[],
-        "u2TaskNumberList":[]
-    }
-    userName=input("enter first user's user name : ")
-    u1=User(userName)
-    userName=input("enter first user's user name : ")  #
-    u2=User(userName)
-    userOption=int(input("Select The User Number (1/2)\n"))  #
-    if userOption==1:
-        user=u1
-    else:
-        user=u2
-    
     try:
         with open(defaultFileName,"r") as file:
             userData=json.load(file)
     except:
         userData={
-            "u1":[],
-            "u2":[],
-            "u1TaskNumberList":[],
-            "u2TaskNumberList":[]
         }
 
-
-
-
+    userName=input("enter first user's user name : ")
+    if userName not in userData:
+        userData[userName]=[]
+        userData[userName+".TaskNumberList"]=[]
+    u1=User(userName)
+    user=u1
 
     while True:
-        option=int(input(f"Active User = {user.name}\nselect One option\n1) Add New Task\n2) List Task\n3) Mark Task As Completed\n4) DeleteTask\n5) Exit To-do List\n6) User Change\n"))
+        option=int(input(f"Active User = {user.name}\nselect One option\n1) Add New Task\n2) List Task\n3) Mark Task As Completed\n4) DeleteTask\n5) Exit To-do List\n"))
         if option==1:
             taskNumber=int(input("Enter Task Number : "))
-            if user==u1:
-                while len(userData["u1TaskNumberList"])!=0 and taskNumber in userData["u1TaskNumberList"]:
-                    taskNumber=int(input("Same Number TAsk already Exists Please Enter Unique Task Number : "))
-            else:
-                while len(userData["u2TaskNumberList"])!=0 and taskNumber in userData["u2TaskNumberList"]:
-                    taskNumber=int(input("Same Number TAsk already Exists Please Enter Unique Task Number :  "))
+            while len(userData[userName+".TaskNumberList"])!=0 and taskNumber in userData[userName+".TaskNumberList"]:
+                taskNumber=int(input("Same Number TAsk already Exists Please Enter Unique Task Number : "))
             taskDescription=input("enter Task Description :\n")
             t1=Task(taskNumber,taskDescription)
-            if user==u1:
-                userData["u1"].append(t1.__dict__)
-                userData["u1TaskNumberList"].append(taskNumber)
-            else:
-                userData["u2"].append(t1.__dict__)
-                userData["u2TaskNumberList"].append(taskNumber)
+            userData[userName].append(t1.__dict__)
+            userData[userName+".TaskNumberList"].append(taskNumber)
         elif option==2:
-            if user==u1:
-                userTask=userData["u1"]
-            else:
-                userTask=userData["u2"]
+            userTask=userData[userName]
+            if len(userTask)==0:
+                print("No Task Added yet !!!\n\n")
+                continue
             for task in userTask:
                 if task["status"]==1:
                     status="Done"
@@ -77,47 +52,27 @@ def startTodoFucntion():
                 print(f"Task Number : {task['number']}\nTask Description : {task['description']}\nTask Status : {status}\nCreated At : {time.asctime(time.localtime(task['createdAt']))}\n")
         elif option==3:
             taskNumber=int(input("\nEnter task number to update : "))
-            if user==u1:
-                if taskNumber not in userData["u1TaskNumberList"]:
-                    print("Task with Given Number Does not Exists !!!")
-                    continue
-                else:
-                    for task in userData["u1"]:
-                        if task["number"]==taskNumber:
-                            task["status"]=1
+            
+            if taskNumber not in userData[userName+".TaskNumberList"]:
+                print("Task with Given Number Does not Exists !!!")
+                continue
             else:
-                if taskNumber not in userData["u2TaskNumberList"]:
-                    print("Task with Given Number Does not Exists !!!")
-                    continue
-                else:
-                    for task in userData["u2"]:
-                        if task["number"]==taskNumber:
-                            task["status"]=1
+                for task in userData[userName]:
+                    if task["number"]==taskNumber:
+                        task["status"]=1
         elif option==4:
-            taskNumber=int(input("\nEnter task number to update : "))
+            taskNumber=int(input("\nEnter task number to Delete : "))
             deleteIndex=None
-            if user==u1:
-                if taskNumber not in userData["u1TaskNumberList"]:
-                    print("Task with Given Number Does not Exists !!!")
-                    continue
-                else:
-                    for index,task in enumerate(userData['u1']):
-                        if task["number"]==taskNumber:
-                            deleteIndex=index
-                    del userData["u1"][deleteIndex]
-                    userData["u1TaskNumberList"].remove(taskNumber)
+            if taskNumber not in userData[userName+".TaskNumberList"]:
+                print("Task with Given Number Does not Exists !!!")
+                continue
             else:
-                if taskNumber not in userData["u2TaskNumberList"]:
-                    print("Task with Given Number Does not Exists !!!")
-                    continue
-                else:
-                    for index,task in enumerate(userData["u2"]):
-                        if task["number"]==taskNumber:
-                            deleteIndex=index
-                    del userData["u2"][deleteIndex]
-                    userData["u2TaskNumberList"].remove(taskNumber)
+                for index,task in enumerate(userData[userName]):
+                    if task["number"]==taskNumber:
+                        deleteIndex=index
+                del userData[userName][deleteIndex]
+                userData[userName+".TaskNumberList"].remove(taskNumber)
         elif option==5:
-            print(userData)
             try:
                 with open(defaultFileName,"a+") as f:
                     f.seek(0)
@@ -128,11 +83,6 @@ def startTodoFucntion():
             except:
                 print("something went wrong !!!")
                 break
-        elif option==6:
-            userOption=int(input("enter user number 1/2\n"))
-            if userOption==1:
-                user=u1
-            else:
-                user=u2
+
 
 startTodoFucntion()
